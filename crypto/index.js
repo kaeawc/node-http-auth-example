e6dccf57a70be1d8d481e1a5b222de2eb77f9396
Jason Pearson <jason.d.pearson@gmail.com>
2013-11-18 14:25:12 -0500

    using proper application secret keys

--- crypto/index.js
+++ crypto/index.js
@@ -1,7 +1,9 @@
 crypt = require('crypto');
 
+secret = require('../config').application.secret
+
 var createSalt = function(callback) {
-  crypt.randomBytes(256, function(error, salt) {
+  crypt.randomBytes(128, function(error, salt) {
 
     if (error) console.log("Failed make random bytes.")
     else callback(salt.toString('hex'));
@@ -9,8 +11,26 @@ var createSalt = function(callback) {
 }
 
 var useSalt = function(password, salt, callback) {
-  crypt.pbkdf2(new Buffer(password, 'hex'),new Buffer(salt, 'hex'),1000,256,callback);
+  crypt.pbkdf2(new Buffer(password, 'hex'),new Buffer(salt, 'hex'),1000,128,callback);
+}
+
+var encrypt = function(plainText) {
+  console.log("Encrypting...")
+  var cipher = crypt.createCipher('aes-256-cbc',secret.key,secret.iv)
+  var encrypted = cipher.update(plainText, 'utf8', 'base64');
+  encrypted += cipher.final('base64');
+  return encrypted;
+}
+
+var decrypt = function(cipherText) {
+  console.log("Decrypting...")
+  var cipher = crypt.createDecipher('aes-256-cbc',secret.key,secret.iv);
+  var plainText  = cipher.update(cipherText, 'base64', 'utf8');
+  plainText += cipher.final('utf8');
+  return plainText;
 }
 
 exports.createSalt   = createSalt
 exports.useSalt      = useSalt
+exports.encrypt      = encrypt
+exports.decrypt      = decrypt
