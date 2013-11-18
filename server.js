593b962112d071df361b9cd76cac6236e53c07fb
Jason Pearson <jason.d.pearson@gmail.com>
2013-11-18 00:04:35 -0500

    most basic routing ever for securing content

--- server.js
+++ server.js
@@ -2,13 +2,53 @@ var http = require('http');
 
 var fs = require('fs');
 
-var landing = fs.readFileSync('views/index.html');
+var pages = {
+  landing : fs.readFileSync('views/landing.html'),
+  dashboard : fs.readFileSync('views/dashboard.html'),
+  error : {
+    unauthorized : fs.readFileSync('views/error/unauthorized.html')
+  }
+}
 
 var listener = function(request,response) {
 
+  console.log(request.url);
+
+  if (authorized(request)) {
+
+    console.log("authorized");
+
+    response.writeHead(200, {});
+
+    response.end(pages.landing);
+
+  } else {
+
+    console.log("unauthorized");
+
+    unauthorized(response);
+  }
+}
+
+var unauthorized = function(response) {
+
+  response.writeHead(401, {});
+
+  response.end(pages.error.unauthorized);
+
+}
+
+var dashboard = function(response) {
+
   response.writeHead(200, {});
 
-  response.end(landing);
+  response.end(pages.dashboard);
+
+}
+
+var authorized = function(request) {
+
+  return (request.url == "/");
 
 }
 
--- test/routes.js
+++ test/routes.js
@@ -2,18 +2,24 @@ assert = require('assert');
 http = require('http');
 
 describe('http', function() {
-  it('should answer with "hello world"', function() {
-    var options = {
-      host:"localhost",
-      port:8000,
-      path:"/"
-    }
-
-    http.get(options, function(response) {
+  it('should answer with "hello world"', function(done) {
+    http.get("http://localhost:8000", function(response) {
       assert.equal(response.statusCode, 200);
       response.setEncoding('utf8');
       response.on('data', function(html) {
         assert.ok(html.indexOf("Hello World"));
+        done();
+      });
+    });
+  });
+
+  it('should return unauthorized when requesting secure content without credentials', function(done) {
+    http.get("http://localhost:8000/dashboard", function(response) {
+      assert.equal(response.statusCode, 401);
+      response.setEncoding('utf8');
+      response.on('data', function(html) {
+        assert.ok(html.indexOf("Sorry, You are Not Authorized To View This Content"));
+        done();
       });
     });
   });
